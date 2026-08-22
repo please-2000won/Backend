79e3cb41c75b8f37dbd1ad06e72ddd795ba962cd
j2nooh <wisestorm01@gmail.com>
2026-08-22 21:49:12 +0900

    chore: 백엔드 초기 설정

---
+++ src/test/java/com/example/peerfolio/PeerfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peerfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
